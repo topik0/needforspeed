c1045417d4c94bfeeaceb81ddea4bf73cf1e96c7
Topik0 <topik@topik.tech>
2021-01-08 16:19:36 -0600

    Moved almost all functions to its own class, added Hardware genesis support

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team9113/Robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team9113/Robot/Robot.java
@@ -2,160 +2,45 @@ package org.firstinspires.ftc.teamcode.Team9113.Robot;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.apache.commons.lang3.time.StopWatch;
-
-import java.util.concurrent.TimeUnit;
 
 @Config
 public class Robot extends LinearOpMode {
     /*
     Robot constructor
      */
-    private HardwareMap hwMap;
+    public HardwareMap hwMap;
+    public HardwareGenesis genesis;
     public Drivetrain drivetrain;
     public Flywheels flywheels;
     public Flicker flicker;
-    public Servo flap, claw, wobble, intakeStopper;
-    public DcMotor leftIntake, rightIntake;
-    public boolean intakeRunning, intakeReversed;
-    public boolean clawClosed = true, wobbleUp = true;
-    double flapPosition;
-    public static double flapHighGoal = .2785, flapPowerShot = .3;
-    public static int shootDelay = 80;
-    public static double clawClosePosition = .42;
+    public Arm arm;
+    public Flap flap;
+    public Claw claw;
+    public Intake intake;
+    public Servo intakeStopper;
 
     public Robot(HardwareMap hwMap) {
         this.hwMap = hwMap;
-        drivetrain = new Drivetrain(hwMap);
-        flywheels = new Flywheels(hwMap);
-        flicker = new Flicker(hwMap, drivetrain.mecanumDrive);
-        // pref = new RobotPreferences();
-        initHardware();
-        flapPosition = flap.getPosition();
-    }
-
-    public Robot(HardwareMap hwMap, boolean noDrivetrain) {
-        this.hwMap = hwMap;
-        flicker = new Flicker(hwMap, drivetrain.mecanumDrive);
-        initHardware();
-        flapPosition = flap.getPosition();
-    }
-
-    /*
-    Initializes the robot and the components
-     */
-    public void initHardware() {
-        flap = hwMap.servo.get("flap");
-        claw = hwMap.servo.get("claw");
-        wobble = hwMap.servo.get("wobble");
-        intakeStopper = hwMap.servo.get("intakeStopper");
-        leftIntake = hwMap.dcMotor.get("leftIntake");
-        rightIntake = hwMap.dcMotor.get("rightIntake");
+        genesis = new HardwareGenesis(hwMap);
+        arm = new Arm(genesis);
+        flap = new Flap(genesis);
+        claw = new Claw(genesis);
+        intake = new Intake(genesis);
+        drivetrain = new Drivetrain(genesis);
+        flywheels = new Flywheels(genesis);
+        flicker = new Flicker(genesis);
     }
 
     public void startPositions() {
-        flap.setPosition(flapHighGoal);
-        wobbleUp();
+        flap.setStartPosition();
+        arm.setStartPosition();
         flicker.startPosition();
-        claw.setPosition(clawClosePosition);
+        claw.setStartPosition();
         intakeStopper.setPosition(.85);
-    }
-
-    public void startPositions(boolean isAuto) {
-        flap.setPosition(.1);
-        flicker.startPosition();
-        claw.setPosition(clawClosePosition);
-        intakeStopper.setPosition(.85);
-        wobbleUp();
-    }
-
-    public void wobbleUp() {
-        wobble.setPosition(.69);
-        wobbleUp = true;
-    }
-
-    public void wobbleDown() {
-        wobble.setPosition(.07);
-        wobbleUp = false;
-    }
-
-    public void intakeDown() {
-        intakeStopper.setPosition(1.0);
-    }
-
-    public void toggleWobble() {
-        if (wobbleUp)
-            wobbleDown();
-        else wobbleUp();
-    }
-
-    public void startIntake() {
-        setIntakePower(1);
-        intakeRunning = true;
-        intakeReversed = false;
-    }
-
-    public void stopIntake() {
-        setIntakePower(0);
-        intakeRunning = false;
-        intakeReversed = false;
-    }
-
-    public void flapAdjustUp() {
-        flapPosition = flap.getPosition();
-        flap.setPosition(flapPosition - .005);
-    }
-
-    public void flapAdjustDown() {
-        flapPosition = flap.getPosition();
-        flap.setPosition(flapPosition + .005);
-    }
-
-    public void flapUpperPosition() {
-        flap.setPosition(flapHighGoal);
-        flapPosition = flap.getPosition();
-    }
-
-    public void flapLowerPosition() {
-        flap.setPosition(flapPowerShot);
-        flapPosition = flap.getPosition();
-    }
-
-    public void openClaw() {
-        claw.setPosition(.74);
-        clawClosed = false;
-    }
-
-    public void closeClaw() {
-        claw.setPosition(clawClosePosition);
-        clawClosed = true;
-    }
-
-    public void toggleClaw() {
-        if (clawClosed)
-            openClaw();
-        else closeClaw();
-    }
-
-    public void setIntakePower(double power) {
-        leftIntake.setPower(power);
-        rightIntake.setPower(power);
-    }
-
-    public void toggleIntake() {
-        if (intakeRunning)
-            stopIntake();
-        else
-            startIntake();
-    }
-
-    public void reverseIntake() {
-        setIntakePower(-1);
-        intakeReversed = true;
     }
 
     public void delayWithFlywheelPID(double milliseconds) {
