5c2a8490e910d53fcd1579aa1353f71b093d2954
Topik0 <topik@topik.tech>
2021-01-08 00:11:54 -0600

    Modified lateral distance and the X multiplier

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team9113/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team9113/drive/StandardTrackingWheelLocalizer.java
@@ -7,6 +7,7 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.localization.ThreeTrackingWheelLocalizer;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
 import org.firstinspires.ftc.teamcode.Team9113.util.Encoder;
 
 import java.util.Arrays;
@@ -31,10 +32,10 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 0.98; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 14.57985; // in; distance between the left and right wheels
+    public static double LATERAL_DISTANCE = 14.51; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = 0.522; // in; offset of the lateral wheel
 
-    public static double X_MULTIPLIER = 1.01365; // Multiplier in the X direction
+    public static double X_MULTIPLIER = 1.0066; // Multiplier in the X direction
     public static double Y_MULTIPLIER = 1.0016135; // Multiplier in the Y direction
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
