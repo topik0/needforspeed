ae0d5dcd18839f0fffc25546853724e20d18e2e8
Topik0 <topik@topik.tech>
2021-01-09 22:12:38 -0600

    Updated flap powershot position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team9113/Robot/Flap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team9113/Robot/Flap.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class Flap {
     public Servo flap;
     private State state;
-    public static double highGoalPosition = .2785, powerShotPosition = .1, adjustUpThreshold = .005, adjustDownThreshold = .005;
+    public static double highGoalPosition = .2785, powerShotPosition = .3, adjustUpThreshold = .005, adjustDownThreshold = .005;
 
     private enum State {
         HIGH_GOAL,
