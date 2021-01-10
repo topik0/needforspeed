98b88f1a3c28ed015afefa57476b18412f4fc7f3
Topik0 <topik@topik.tech>
2021-01-09 23:48:54 -0600

    Added comments and used robot auto methods to choose the correct start position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team9113/Robot/Flap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team9113/Robot/Flap.java
@@ -3,9 +3,16 @@ package org.firstinspires.ftc.teamcode.Team9113.Robot;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.Servo;
 
+/**
+ * @author Topik
+ * @version 1.0
+ * @since 1.0
+ * <p>
+ * This class controls the movement of the aiming flap
+ */
 @Config
 public class Flap {
-    private Robot robot;
+    private final Robot robot;
     public Servo flap;
     private State state;
     public static double highGoalPosition = .2785, powerShotPosition = .3, adjustUpThreshold = .005, adjustDownThreshold = .005;
@@ -16,6 +23,12 @@ public class Flap {
         NEUTRAL
     }
 
+    /**
+     * Flap constructor that grabs the flap
+     *
+     * @param gen   the HardwareGenesis object needed to grab the flap
+     * @param robot the robot that contains the robot objects
+     */
     public Flap(HardwareGenesis gen, Robot robot) {
         if (gen == null)
             throw new BadInitializationException("Flap genesis is null");
@@ -28,31 +41,56 @@ public class Flap {
         setStartPosition();
     }
 
+    /**
+     * Sets the flap to its stop position
+     */
     public void setStartPosition() {
-        flap.setPosition(powerShotPosition);
-        state = State.POWERSHOT;
+        if (robot.isAuto()) {
+            flap.setPosition(powerShotPosition);
+            state = State.POWERSHOT;
+        } else {
+            flap.setPosition(highGoalPosition);
+            state = State.HIGH_GOAL;
+        }
     }
 
+    /**
+     * Sets the flap to the high goal position
+     */
     public void goToHighGoalPosition() {
         flap.setPosition(highGoalPosition);
         state = State.HIGH_GOAL;
         robot.flywheels.setHighGoalVelocity();
     }
 
+    /**
+     * Sets the flap to the powershot position
+     */
     public void goToPowershotPosition() {
         flap.setPosition(powerShotPosition);
         state = State.POWERSHOT;
         robot.flywheels.setPowershotVelocity();
     }
 
+    /**
+     * Raises the flap up an increment
+     */
     public void adjustUp() {
         flap.setPosition(flap.getPosition() - adjustUpThreshold);
     }
 
+    /**
+     * Lowers the flap an increment
+     */
     public void adjustDown() {
         flap.setPosition(flap.getPosition() + adjustDownThreshold);
     }
 
+    /**
+     * Gets the current
+     *
+     * @return
+     */
     public State getState() {
         return state;
     }
