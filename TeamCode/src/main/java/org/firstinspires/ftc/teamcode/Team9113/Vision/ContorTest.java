340411583e5208708085075a2490d9855c3250ec
Topik0 <topik@topik.tech>
2021-01-09 20:55:51 -0600

    Fixed for use with the Vision class and robot changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team9113/Vision/ContorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team9113/Vision/ContorTest.java
@@ -4,73 +4,23 @@ import com.arcrobotics.ftclib.vision.UGContourRingPipeline;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.teamcode.Team9113.Robot.Robot;
-import org.firstinspires.ftc.teamcode.Team9113.drive.SampleMecanumDrive;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.firstinspires.ftc.teamcode.Team9113.Robot.Vision;
 
 @TeleOp(name = "ContorTest", group = "Linear Opmode")
 public class ContorTest extends LinearOpMode {
-    private static final int CAMERA_WIDTH = 320; // width  of wanted camera resolution
-    private static final int CAMERA_HEIGHT = 240; // height of wanted camera resolution
-
-    private static final int HORIZON = 100; // horizon value to tune
-
-    private static final boolean DEBUG = false; // if debug is wanted, change to true
-
-    private static final boolean USING_WEBCAM = true; // change to true if using webcam
-    private static final String WEBCAM_NAME = "Webcam 1"; // insert webcam name from configuration if using webcam
-
-    private OpenCvCamera camera;
-
     @Override
     public void runOpMode() {
-        Robot robot = new Robot(hardwareMap);
-        robot.startPositions(true);
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        int cameraMonitorViewId = this
-                .hardwareMap
-                .appContext
-                .getResources().getIdentifier(
-                        "cameraMonitorViewId",
-                        "id",
-                        hardwareMap.appContext.getPackageName()
-                );
-        if (USING_WEBCAM) {
-            camera = OpenCvCameraFactory
-                    .getInstance()
-                    .createWebcam(hardwareMap.get(WebcamName.class, WEBCAM_NAME), cameraMonitorViewId);
-        } else {
-            camera = OpenCvCameraFactory
-                    .getInstance()
-                    .createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
-        }
-
-        UGContourRingPipeline pipeline;
-        camera.setPipeline(pipeline = new UGContourRingPipeline(telemetry, DEBUG));
-
-        UGContourRingPipeline.Config.setCAMERA_WIDTH(CAMERA_WIDTH);
-
-        UGContourRingPipeline.Config.setHORIZON(HORIZON);
-
-        camera.openCameraDeviceAsync(() -> camera.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT));
-
-        String height = pipeline.getHeight().toString();
-        String initial = height;
-        while (!isStarted()) {
-            height = pipeline.getHeight().toString();
-            telemetry.addData("Initial Count: ", initial);
-            telemetry.addData("Current count: ", height);
+        Vision vision = new Vision(hardwareMap, telemetry);
+        while (!isStarted() && !vision.isReadyToRead())
+            telemetry.addData("Vision Status", "Not Ready");
+        UGContourRingPipeline.Height height;
+        while (!isStarted() && vision.isReady()) {
+            telemetry.addData("Vision Status", "Not Ready");
+            telemetry.addData("Camera Initialization Time: ", vision.cameraInitTime());
             telemetry.update();
         }
-        String initCount = height;
         while (opModeIsActive()) {
-            height = pipeline.getHeight().toString();
-            telemetry.addData("Initial count: ", initial);
-            telemetry.addData("Initialized count: ", initCount);
+            height = vision.getHeight();
             telemetry.addData("Current count: ", height);
             telemetry.update();
         }
