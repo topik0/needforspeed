74bf1f9218ecffe5c3c331a3896c5b9c8b3e90c8
Topik0 <topik@topik.tech>
2021-01-10 22:37:51 -0600

    Renamed to Vision Test, added documentation comments, fixed logic

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NFS/Vision/VisionTest.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.NFS.Vision;
+
+import com.arcrobotics.ftclib.vision.UGContourRingPipeline;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * @author Topik
+ * @version 1.0
+ * @since 1.0
+ * This class is an OP Mode that runs the robot vision to detect the number of rings in a stack
+ */
+@TeleOp(name = "Vision Test", group = "Linear Opmode")
+public class VisionTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        Vision vision = new Vision(hardwareMap, telemetry);
+        while (!isStarted() && !vision.isReadyToRead())
+            telemetry.addData("Vision Status", "Not Ready");
+        UGContourRingPipeline.Height height;
+        while (!isStarted() && vision.isReady()) {
+            telemetry.addData("Vision Status", "Not Ready");
+            telemetry.addData("Camera Initialization Time: ", vision.cameraInitTime());
+            telemetry.update();
+        }
+        while (opModeIsActive()) {
+            height = vision.getHeight();
+            telemetry.addData("Current count: ", height);
+            telemetry.update();
+        }
+    }
+}
