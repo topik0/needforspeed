5aeae38cd15e5e76ea7327c6229ed0e5be3bcda9
Topik0 <topik@topik.tech>
2021-01-10 22:08:07 -0600

    Added comments and renamed exception messages

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NFS/RobotComponents/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NFS/RobotComponents/Arm.java
@@ -5,9 +5,21 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.NFS.RobotComponents.Exceptions.BadInitializationException;
 
+/**
+ * @author Topik
+ * @version 1.0
+ * @since 1.0
+ * This class controls the wobble arm mechanism
+ */
 @Config
 public class Arm {
+    /**
+     * The arm servo object
+     */
     public Servo arm;
+    /**
+     * The various servo positions
+     */
     public static double startPosition = .69, upPosition = .69, downPosition = .07;
     private State state;
 
@@ -16,41 +28,73 @@ public class Arm {
         DOWN
     }
 
+    /**
+     * Arm Constructor
+     *
+     * @param gen the HardwareGenesis object needed to grab the arm servo
+     */
     public Arm(HardwareGenesis gen) {
         if (gen == null)
-            throw new BadInitializationException("Arm genesis is null");
+            throw new BadInitializationException("Null Genesis detected in Arm");
         arm = gen.arm;
         if (arm == null)
-            throw new BadInitializationException("Arm is null");
+            throw new BadInitializationException("Null arm detected in Arm");
     }
 
+    /**
+     * Sets the arm to the start position
+     */
     public void setStartPosition() {
         arm.setPosition(startPosition);
         state = State.UP;
     }
 
+    /**
+     * Moves the arm to the up position
+     */
     public void up() {
         arm.setPosition(upPosition);
         state = State.UP;
     }
 
+    /**
+     * Moves the arm to the down position
+     */
     public void down() {
         arm.setPosition(downPosition);
         state = State.DOWN;
     }
 
+    /**
+     * Checks if the arm is up
+     *
+     * @return true if the arm is up
+     */
     public boolean isUp() {
         return state == State.UP;
     }
 
-    public State getState(){
+    /**
+     * Gets the state of the arm
+     *
+     * @return the arm state
+     */
+    public State getState() {
         return state;
     }
 
+    /**
+     * Sets the position of the arm
+     *
+     * @param position the position to be set
+     */
     public void setPosition(double position) {
         arm.setPosition(position);
     }
 
+    /**
+     * Toggles the arm state between up and down
+     */
     public void toggle() {
         if (isUp()) down();
         else up();
