10e36fe03cf32e460f54d12075aa4345739bdf9f
Topik0 <topik@topik.tech>
2021-01-10 22:35:57 -0600

    Added documentation comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NFS/RobotComponents/HardwareGenesis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NFS/RobotComponents/HardwareGenesis.java
@@ -8,15 +8,42 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+/**
+ * @author Topik
+ * @version 1.0
+ * @since 1.0
+ * This class contains all of the hardware objects needed for the robot
+ */
 @Config
 public class HardwareGenesis {
+    /**
+     * The hardware map needed to access the robot hardware
+     */
     public HardwareMap hwMap;
+    /**
+     * The servos used on the robot
+     */
     public Servo flap, claw, arm, intakeStopper, flicker;
+    /**
+     * The intake motors
+     */
     public DcMotor leftIntake, rightIntake;
+    /**
+     * The flywheel motors
+     */
     public Motor flywheelFront, flywheelBack;
+    /**
+     * The drivetrain motors
+     */
     public DcMotorEx[] drivetrainMotors;
+    /**
+     * The IMU
+     */
     public BNO055IMU imu;
 
+    /**
+     * Names of all of the robot hardware in the configuration
+     */
     public static String
             flapName = "flap",
             clawName = "claw",
@@ -29,8 +56,16 @@ public class HardwareGenesis {
             flywheelBackName = "flywheelBack",
             imuName = "imu";
 
+    /**
+     * Names of the drivetrain motors in the configuration
+     */
     public static String[] drivetrainNames = {"lowerRight", "lowerLeft", "upperRight", "upperLeft"};
 
+    /**
+     * HardwareGenesis constructor which makes all of the robot hardware objects
+     *
+     * @param hwMap the HardwareMap needed to access robot components
+     */
     public HardwareGenesis(HardwareMap hwMap) {
         this.hwMap = hwMap;
         drivetrainMotors = new DcMotorEx[4];
