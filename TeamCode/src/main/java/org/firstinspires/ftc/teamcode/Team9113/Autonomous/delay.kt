ea8498c86cd6468a7d260f4153d9859b36c799d9
Topik0 <topik@topik.tech>
2021-01-12 13:43:33 -0600

    Removed legacy code which was accidentally added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team9113/Autonomous/delay.kt
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.Team9113.Autonomous
-
-
-fun delay(delay: Long, block: () -> Unit) {
-    GlobalScope.launch {
-        Thread.sleep(delay)
-        block()
-    }
-}
