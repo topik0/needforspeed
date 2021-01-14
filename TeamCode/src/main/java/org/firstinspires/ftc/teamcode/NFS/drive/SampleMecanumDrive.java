57461904e42f202333e8ba8374d875655f550aa9
Topik0 <topik@topik.tech>
2021-01-13 21:07:58 -0600

    Removed useless import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NFS/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NFS/drive/SampleMecanumDrive.java
@@ -28,7 +28,6 @@ import com.acmerobotics.roadrunner.util.NanoClock;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
