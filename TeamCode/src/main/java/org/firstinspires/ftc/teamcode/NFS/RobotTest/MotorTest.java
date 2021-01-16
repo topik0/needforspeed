6a403423a6fce79d7f897c681a5177251c8db4a6
xdevboi <thedevin147@gmail.com>
2021-01-16 01:48:19 -0600

    moved MotorTest.java to RobotTest folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NFS/RobotTest/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NFS/RobotTest/MotorTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.NFS.TeleOP;
+package org.firstinspires.ftc.teamcode.NFS.RobotTest;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
