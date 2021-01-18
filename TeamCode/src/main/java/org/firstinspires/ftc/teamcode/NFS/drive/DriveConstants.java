6a6f1c12c52553eb752950fd9058154fbbd8fb2e
xdevboi <thedevin147@gmail.com>
2021-01-17 22:15:49 -0600

    updated new tuned Drive Constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NFS/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NFS/drive/DriveConstants.java
@@ -32,7 +32,7 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = false;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(12, 0, 9, 15);
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0, 0);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -52,9 +52,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.01386 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0.0045;
-    public static double kStatic = 0.13012;
+    public static double kV = 0.014;
+    public static double kA = 0.0047;
+    public static double kStatic = 0.00001;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -63,8 +63,8 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 70;
-    public static double MAX_ACCEL = 70;
+    public static double MAX_VEL = 62;
+    public static double MAX_ACCEL = 60;
     public static double MAX_ANG_VEL = Math.toRadians(300);
     public static double MAX_ANG_ACCEL = Math.toRadians(300);
 
