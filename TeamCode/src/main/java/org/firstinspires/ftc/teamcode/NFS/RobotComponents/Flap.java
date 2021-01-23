5e83d45c76e2e83c748774ac49e50a6fd91ccf5a
xdevboi <thedevin147@gmail.com>
2021-01-22 18:36:03 -0600

    new flap flush pos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NFS/RobotComponents/Flap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NFS/RobotComponents/Flap.java
@@ -16,7 +16,7 @@ public class Flap {
     /**
      * The various positions of the flap
      */
-    public static double startPosition = 0, highGoalPosition = .3, powerShotPosition = .3, flushPosition = 1, adjustUpThreshold = .005, adjustDownThreshold = .005;
+    public static double startPosition = 0, highGoalPosition = .3, powerShotPosition = .3, flushPosition = .1, adjustUpThreshold = .005, adjustDownThreshold = .005;
     /**
      * The flap Servo object
      */
